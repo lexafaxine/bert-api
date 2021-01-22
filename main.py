f7f583cd6e24f41f76b059ea62457ed815df17e2
lexafaxine <lexafaxine@gmail.com>
2021-01-22 17:29:14 +0900

    fix

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def preprocess(text, label_map, max_seq_length, tokenizer):
     for char in paragraph:
         if char == "。":
             sent.append(char)
+            "".join(sent)
             sentences.append(sent)
             sent = []
         else:
