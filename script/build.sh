19a4b416b7ba4f71e66a1bf5fa5e9e23a8b00401
akishichinibu <akishichinibu@gmail.com>
2021-01-25 16:23:26 +0900

    fix script error

--- script/build.sh
+++ script/build.sh
@@ -1,12 +1,14 @@
+export PROJECT_BASE=$(dirname $(dirname $(readlink -f "$0")))
+
 docker image build \
-  -f docker/python3.6.dockerfile \
+  -f ${PROJECT_BASE}/docker/python3.6.dockerfile \
   -t ubuntu20.python3.6:latest .
 
 docker image build \
-  -f docker/mecab.dockerfile \
+  -f ${PROJECT_BASE}/docker/mecab.dockerfile \
   -t "mecab-python3":latest .
 
 docker image build \
   --no-cache \
-  -f docker/server.dockerfile \
+  -f ${PROJECT_BASE}/docker/server.dockerfile \
   -t "bert-server":latest .
